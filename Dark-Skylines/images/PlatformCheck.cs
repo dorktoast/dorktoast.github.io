160c9d2eab9b0972e0104335f9e1fb5cb7fcf744
DorkToast <sam@gib.games>
2023-08-25 18:19:02 -0600

    bbUpdates

---
+++ Dark-Skylines/images/PlatformCheck.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformCheck : UdonSharpBehaviour
+{
+#if UNITY_ANDROID
+    public bool isAndroid = true;
+#else
+    public bool isAndroid = false;
+#endif
+
+    public Toggle CheckToggle;
+    
+    private void Start()
+    {
+        if (isAndroid) Debug.Log("UNITY_ANDROID preprocessor reads as TRUE");
+        else Debug.Log("UNITY_ANDROID preprocessor reads as FALSE");
+
+        CheckToggle.SetIsOnWithoutNotify(isAndroid);
+    }
+}
